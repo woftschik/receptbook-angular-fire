94a0d0ca452cac5b837146c4fa7e9f4baaf23ec8
Waldemar Mattheis <dev@w-matthies.de>
2023-08-05 17:47:11 -0400

    add modal and shoppin-list comp

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.modal';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a simply a test', 'https://cdn.pixabay.com/photo/2017/07/16/12/07/beef-2509104_1280.jpg'),
+    new Recipe('A Test Recipe', 'This is a simply a test', 'https://cdn.pixabay.com/photo/2017/07/16/12/07/beef-2509104_1280.jpg')
+  ];
+  message: string = 'Hallo von der Komponente!';
 }
