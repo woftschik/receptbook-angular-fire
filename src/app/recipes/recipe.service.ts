ff309d751919f012644b4e6dfc75e95370f7dd48
woftschik <waldemar.matthies@gmx.de>
2023-08-30 06:53:41 -0400

    fix name bug

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,8 @@
-export class RecipeService {
-    
+ import { Recipe } from './recipe.model'
+ 
+ export class RecipeService {
+    recipes: Recipe[] = [
+        new Recipe('Rindfleisch Steak', 'Dieses Rezept zeigt euch, wie dieses Steak perfekt wird.', 'https://cdn.pixabay.com/photo/2017/07/16/12/07/beef-2509104_1280.jpg'),
+        new Recipe('Kalbssteak', 'Mit grünen Spargeln und Morcheln. Schmeckt genial fein.', 'https://cdn.pixabay.com/photo/2017/03/23/19/57/asparagus-2169305_1280.jpg')
+      ];
 }
